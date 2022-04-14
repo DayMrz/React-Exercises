fa84b1fc6a2c9a3482179229028e633047274194
Leily <leilymrz@gmail.com>
2022-04-13 23:38:17 -0700

    class link react

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,18 +1,20 @@
+import { useState } from 'react'
+
 import Pokedex from './components/Pokedex'
 import Pokecard from './components/Pokecard'
 import './App.css';
 
 function App() {
+  const [id, setId] = useState(true)
+
   return (
     <div className="App">
-      <h1> Pokedex</h1>
+      <h1>Pokedex</h1>
+      <h2>Current id: {id}</h2>
       <Pokedex />
-      <Pokecard
-        id={4}
-        name="Charmander"
-        type="fire"
-        exp={62}
-      />
+      {id && <Pokecard id={1} />}
+
+      <button type="button" onClick={() => setId(currentId => !currentId)}>toggle</button>
     </div>
   );
 }
--- pokedex/src/components/Pokecard/Pokecard.js
+++ pokedex/src/components/Pokecard/Pokecard.js
@@ -1,15 +1,60 @@
+import { useState, useEffect } from 'react'
+
+// https://reactjs.org/docs/hooks-reference.html
+// useState*
+// useEffect
+// useCallback
+// useRef*
+// useReducer
+// useId
+// useContext
+// useMemo
+// useTransition
+
 import "./Pokecard.css"
-const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail";
+
+const POKE_ASSET = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail";
+const POKE_API = "https://pokeapi.co/api/v2/pokemon";
 
 function Pokecard(props) {
-  const imgSrc = `${POKE_API}/${String(props.id).padStart(3, '0')}.png`;
+  const [data, setData] = useState({
+    name: '',
+    type: '',
+    exp: '',
+    image: '',
+  })
+
+  const { id } = props;
+  const { name, type, exp, image } = data;
+
+  useEffect(
+    () => {
+      setData(prevValue => {
+        return ({ ...prevValue, image: `${POKE_ASSET}/${String(id).padStart(3, '0')}.png` })
+      })
+
+      fetch(`${POKE_API}/${id}`)
+        .then(res => res.json())
+        .then(res => {
+          setData(prevValue => {
+            return ({
+              ...prevValue,
+              name: res.name,
+              type: res.types.map(({ type: { name } }) => name).join('/'),
+              exp: res.base_experience,
+            })
+          })
+        })
+    },
+    [id]
+  )
 
   return (
     <div className="Pokecard">
-      <h1>{props.name}</h1>
-      <img src={imgSrc} alt={props.name} />
-      <div>Type: {props.type}</div>
-      <div>EXP: {props.exp}</div>
+      <h1>{name}</h1>
+      <img src={image} alt={name} />
+      <div>Type: {type}</div>
+      <div>EXP: {exp}</div>
     </div>
   )
 }
